b86c4a51a8b7c968a82c9744b0decde8ed2648e0
joannabutkiewicz <joanna.butkiewicz@profil-software.com>
2020-11-27 16:43:58 +0100

    DF-3210: Add history of object to django admin from auditlog

--- auditlog_tests/admin.py
+++ auditlog_tests/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 
+from auditlog.mixins import AuditlogAdminHistoryMixin
 from auditlog.registry import auditlog
+from auditlog_tests.models import SimpleModel
+
+
+class SimpleModelAdmin(AuditlogAdminHistoryMixin, admin.ModelAdmin):
+    list_display = ['text']
+
 
 for model in auditlog.get_models():
-    admin.site.register(model)
+    if model == SimpleModel:
+        admin.site.register(model, SimpleModelAdmin)
+    else:
+        admin.site.register(model)
