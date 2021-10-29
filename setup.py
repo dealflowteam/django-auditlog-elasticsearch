3cb5106b4638cd929e0d45cdf5feefb543f016db
Kamil Waldoch Profil Software <38035994+kamilprofil@users.noreply.github.com>
2021-10-29 11:04:43 +0200

    added elastic search to setup.py

--- setup.py
+++ setup.py
@@ -23,7 +23,9 @@ setup(
     install_requires=[
         'django-jsonfield>=1.0.0',
         'python-dateutil>=2.6.0',
+        'elasticserach==7.12',
         'elasticsearch-dsl==7.3.0',
+
     ],
     zip_safe=False,
     classifiers=[
